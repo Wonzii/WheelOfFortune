40eb009d53e605eb8469b7a0cd9c81e2cc8c4f86
student <student@admins-MacBook-Pro-8.local>
2020-06-16 17:02:26 -0700

    WheelOfFortune(updated)

--- app/src/main/java/com/example/wheeloffortune/LeaderboardActivity.java
+++ app/src/main/java/com/example/wheeloffortune/LeaderboardActivity.java
@@ -5,13 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class LeaderboardActivity extends AppCompatActivity {
+    EditText editText;
+    ListView leadLV;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    String name;
+    ArrayList<String> nameArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference().child("name");
+        editText = findViewById(R.id.edittextName);
+        leadLV = findViewById(R.id.leaderboardLV);
     }
 
     public void openMainActivity(View view) {
@@ -22,5 +39,10 @@ public class LeaderboardActivity extends AppCompatActivity {
     public void openPlayActivity(View view) {
         Intent intent = new Intent(LeaderboardActivity.this, PlayActivity.class);
         startActivity(intent);
+    }
+
+    public void addToLeaderBoard(View view) {
+        name = editText.getText().toString();
+        nameArrayList.add(name);
     }
 }
